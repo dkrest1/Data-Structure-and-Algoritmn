8c3d3e5a10809955a993f8d133df6d5050ea0eb1
Oluwatosin Akande <aktosin1@gmail.com>
2023-07-20 15:35:51 +0100

    two pointer: triplet sum less than a target

--- coding-pattern/python/two-pointers.py
+++ coding-pattern/python/two-pointers.py
@@ -203,6 +203,44 @@ def closest_triplet_sum(arr, targetSum):
 result_6 = closest_triplet_sum([1, 0, 1, 1], 100)
 
 
+#//////////////////////////////// Triplet With Smaller Sum ////////////////////////////////////////////////
+# Problem Statement #
+# Given an array arr of unsorted numbers and a target sum, count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. Write a function to return the count of such triplets.
+
+# Example 1:
+
+# Input: [-1, 0, 2, 3], target=3 
+# Output: 2
+# Explanation: There are two triplets whose sum is less than the target: [-1, 0, 3], [-1, 0, 2]
+# Example 2:
+
+# Input: [-1, 4, 2, 1, 3], target=5 
+# Output: 4
+# Explanation: There are four triplets whose sum is less than the target: 
+#    [-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]
+
+def triplet_less_than_target_sum(arr, target):
+    arr.sort()
+    count = 0
+    for i in range(len(arr) -2):
+        count += search_pair_less(arr,target - arr[i], i)
+    return count
+
+def search_pair_less(arr, target_sum, first):
+    count = 0
+    left , right = first + 1,  len(arr) - 1
+    
+    while(left < right):
+        if arr[left] + arr[right] < target_sum:
+            count += right - left
+            left += 1
+        else:
+            right -= 1
+    return count
+
+result_7 = triplet_less_than_target_sum([-1, 0, 2, 3], 3)
+
+
 
 def main():
     print("example_one: ", result_1)
@@ -211,6 +249,7 @@ def main():
     print("example_4", result_4)
     print("example_5", result_5)
     print("example_6", result_6)
+    print("example_7", result_7)
 
 
 main()
