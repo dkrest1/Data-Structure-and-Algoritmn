50cab93432229e00b1aa656a1e3d8ecd4c5d60f9
Oluwatosin Akande <aktosin1@gmail.com>
2023-08-15 20:53:16 +0100

    update

--- coding-pattern/javascript/breath-first-search.js
+++ coding-pattern/javascript/breath-first-search.js
@@ -89,7 +89,6 @@ function traverse(root) {
 
     return result
 }
-
 // ///////////////////////// Reverse Level Order Traversal (easy) ///////////////////////////////////
 // Problem Statement #
 // Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first. You should populate the values of all nodes in each level from left to right in separate sub-arrays.
